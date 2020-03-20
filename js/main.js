15346e67dc20fbe5445b773cfc2a94a1e234dbb1
khenryjoe <joseph.ketterer@gmail.com>
2020-03-20 11:35:31 +0000

    add individual json files

--- js/main.js
+++ js/main.js
@@ -22,9 +22,13 @@ $(document).on("pagecontainerbeforeshow", function (){
 
 $(document).on('pagebeforeshow', '#properties', function(){
     const x = getProperties();
+    let h =''
     for(i of x){
-        console.log(i)
+        h += '<p>' + i.name + '</p>'
+        h +=  '<p>' + i.shortdescription + '</p>'
     }
+    console.log(h)
+    $("#houses").html(h)
   });
 
 
@@ -33,3 +37,25 @@ function  getProperties() {
     let properties = JSON.parse(localStorage.getItem('result'));
 	return properties ;
 }
+
+
+// $(document).on("pagecontainerbeforeshow", function (e, ui) {
+// 	let page = ui.toPage[0].id;
+// 	if( page == "five" ) {
+// 		$.get("data/hotels.json", function(result, status) {
+// 		let hotel = "";
+// 		for (let i = 0; i < result.length; i++) {
+// 			hotel += "<li><a href='hotel.html?id=" + 
+// 			result[i].id + 
+// 			"'><img src='images/" + 
+// 			result[i].thumbnail + 
+// 			"'>" + 
+// 			result[i].name + 
+// 			"<p>" + 
+// 			result[i].shortdescription +
+// 			"</p></a></li>";
+// 		}
+// 		$("#hotellist").html(hotel).listview("refresh");
+// 		}, "json");	
+// 	}
+// });
