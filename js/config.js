ccea06f60e778666e5cbb4c23ef6efae89c57f6e
khenryjoe <joseph.ketterer@gmail.com>
2020-03-21 07:26:33 +0000

    add links to properties for each id

--- js/config.js
+++ js/config.js
@@ -1,17 +1,5 @@
-// 
+//
 
-$(document).bind('mobileinit', function () {
-    $.mobile.page.prototype.options.theme = "a";
-    // $.get("../data/houses.json", function (result, status) {
-    //         if (typeof (Storage) !== "undefined") {
-    //             localStorage.setItem("result", JSON.stringify(result));
-    //         } else {
-    //             console.log('error')
-    //         }
-    //     })
-    //     .fail(function () {
-    //         console.log("error");
-    //     })
-
-
+$(document).bind("mobileinit", function() {
+  $.mobile.page.prototype.options.theme = "a";
 });
--- js/main.js
+++ js/main.js
@@ -6,45 +6,52 @@
 
 // })
 
-
-
-$(document).on("pagecontainerbeforeshow", function (){
- 
-  
-    console.log( "ready!" );
-    $('#favourite').on('click', function (){
-        const x  = 'orchard'
-        localStorage.setItem('fave', JSON.stringify(x))
-    })
-})
-
-
-
-$(document).on('pagebeforeshow', '#properties', function(){
-    $.get("../data/houses.json", function (result, status) {
-        console.log(result)
-        let h =''
-        $.each(result, function(i, v) {
-        
-            h += '<li> <a href="#propertydetail" data-transition="slidefade">'    + v.name + '</a></li>'
-            h +=  '<li> <a href="#first" data-transition="slidefade">' + v.shortdescription + '</a></li>'
-        })
-        
-        $("#houses").html(h)
-
-       let p = result.filter(function(i){
-            return i.id == 3;
-        })
-    })
-    .fail(function (status){
-        const h = status.status + ' error. There was an error retreiving data'
-        $("#houses").html(h)
-    })
-    var urlParams = new URLSearchParams(window.location.search);
-    console.log(urlParams)
-   
+$(document).on("pagecontainerbeforeshow", function() {
+  console.log("ready!");
+  $("#favourite").on("click", function() {
+    const x = "orchard";
+    localStorage.setItem("fave", JSON.stringify(x));
   });
+});
 
+$(document).on("pagebeforeshow", "#properties", function() {
+  $.get("../data/houses.json", function(result, status) {
+    console.log(result);
+    let h = "";
+    $.each(result, function(i, v) {
+      h +=
+        `<li> <a  href="orchard.html?post=${v.id}" data-transition="slidefade"> 
+        ${v.name} 
+        </a></li>`;
+      h +=
+        `<li> <a href="orchard.html?post=${v.id}" data-transition="slidefade" > 
+        ${v.shortdescription} 
+        </a></li>`;
+    });
+
+    $("#houses").html(h);
+  }).fail(function(status) {
+    const h = status.status + " error. There was an error retreiving data";
+    $("#houses").html(h);
+  });
+});
+
+$(document).on("pagebeforeshow", "#orchard", function() {
+  var urlParams = new URLSearchParams(window.location.search);
+  const y = urlParams.get("post");
+
+  $.get("../data/houses.json", function(result, status) {
+    const p = result.filter(function(i) {
+      return i.id == y;
+    });
+    console.log(p)
+
+  })
+  .fail(function(status) {
+    const h = status.status + " error. There was an error retreiving data";
+    console.log(h);
+  });
+});
 
 // FOR PARAMS
 
@@ -54,24 +61,23 @@ $(document).on('pagebeforeshow', '#properties', function(){
 //     //query is now an ID, do stuff with it...
 //   });
 
-
 // $(document).on("pagecontainerbeforeshow", function (e, ui) {
 // 	let page = ui.toPage[0].id;
 // 	if( page == "five" ) {
 // 		$.get("data/hotels.json", function(result, status) {
 // 		let hotel = "";
 // 		for (let i = 0; i < result.length; i++) {
-// 			hotel += "<li><a href='hotel.html?id=" + 
-// 			result[i].id + 
-// 			"'><img src='images/" + 
-// 			result[i].thumbnail + 
-// 			"'>" + 
-// 			result[i].name + 
-// 			"<p>" + 
+// 			hotel += "<li><a href='hotel.html?id=" +
+// 			result[i].id +
+// 			"'><img src='images/" +
+// 			result[i].thumbnail +
+// 			"'>" +
+// 			result[i].name +
+// 			"<p>" +
 // 			result[i].shortdescription +
 // 			"</p></a></li>";
 // 		}
 // 		$("#hotellist").html(hotel).listview("refresh");
-// 		}, "json");	
+// 		}, "json");
 // 	}
 // });
