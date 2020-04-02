0bc83e312ad9674db8fcf233d41e071bc937e66b
khenryjoe <joseph.ketterer@gmail.com>
2020-04-02 07:20:08 +0100

    post code to json file

--- js/main.js
+++ js/main.js
@@ -1,123 +1,131 @@
 // #2A6793;
 
-
-
-$(document).on("pagecontainerbeforeshow", function (e, ui) {
-  const thisPage = $.mobile.pageContainer.pagecontainer('getActivePage').attr('id');
-  if (thisPage == 'orchard') {
+$(document).on("pagecontainerbeforeshow", function(e, ui) {
+  const thisPage = $.mobile.pageContainer
+    .pagecontainer("getActivePage")
+    .attr("id");
+  if (thisPage == "orchard") {
     const urlParams = new URLSearchParams(window.location.search);
     const postid = urlParams.get("post");
     getMarked(postid);
     const popupwidth = $(window).width() * 0.9;
 
     // use counter as a value for the array.
-    $.get("../data/houses.json", function (result, status) {
-      const p = result.filter(i => i.id == postid);
-       
-      // MB USE APPEND
-      $("#room-info")
-        .html(p[0].longdescription)
-        .trigger("create");
-      $("#area-info")
-        .html(p[0].area)
-        .trigger("create");
-      $("#price")
-        .html(`Price ${p[0].price}`)
-        .trigger("create");
-      
-       
-    
-        const tel = `<a href="tel:+${p[0].telephone}" class="ui-btn ui-icon-phone à ui-btn-icon-left ui-corner-all ui-btn-icon-notext">Call advertiser</a>`;
-      $('#telephone')
-        .html(tel)
-        
-        $('#address')
-        .html(p[0].address)
-        
-        $('#email')
-        .html(p[0].email)
-        $("#contact").listview('refresh');
-     
-      $("#unfavourite").on("tap", function () {
-        removeFaves(p[0].id);
-        getMarked(postid);
-      });
+    $.get(
+      "../data/houses.json",
+      function(result, status) {
+        const p = result.filter(i => i.id == postid);
 
-      $("#favourite").on("tap", function () {
-        addFaves(p);
-        getMarked(postid);
-      });
-      $("#popupImage").css('width', popupwidth)
-      let counter = 1;
+        // MB USE APPEND
+        $("#room-info")
+          .html(p[0].longdescription)
+          .trigger("create");
+        $("#area-info")
+          .html(p[0].area)
+          .trigger("create");
+        $("#price")
+          .html(`Price ${p[0].price}`)
+          .trigger("create");
 
-      function getImage(counter) {
-        return newImage = `../images/large/${p[0].large[counter]}`;
-      }
-      $("#popupImage").css('background-image', `url(' ${getImage(counter)}')`)
+        const tel = `<a href="tel:+${
+          p[0].telephone
+        }" class="ui-btn ui-icon-phone à ui-btn-icon-left ui-corner-all ui-btn-icon-notext">Call advertiser</a>`;
+        $("#telephone").html(tel);
 
-      $("#popupImage").on('swipeleft', function () {
+        $("#address").html(p[0].address);
 
-        switch (true) {
-          case counter == 1:
+        $("#email").html(p[0].email);
+        $("#contact").listview("refresh");
 
-            counter--;
-            $("#popupImage").css('background-image', `url(' ${getImage(counter)}')`)
-            $("#right").fadeOut();
+        $("#unfavourite").on("tap", function() {
+          removeFaves(p[0].id);
+          getMarked(postid);
+        });
 
-            break;
+        $("#favourite").on("tap", function() {
+          addFaves(p);
+          getMarked(postid);
+        });
+        $("#popupImage").css("width", popupwidth);
+        let counter = 1;
 
-          case counter == 2:
-
-            counter--;
-            $("#popupImage").css('background-image', `url(' ${getImage(counter)}')`)
-            $("#left").fadeIn();
-            $("#right").fadeIn();
-
-            break;
-
-          case counter == 0:
-            break;
+        function getImage(counter) {
+          return (newImage = `../images/large/${p[0].large[counter]}`);
         }
+        $("#popupImage").css(
+          "background-image",
+          `url(' ${getImage(counter)}')`
+        );
 
-      })
-      $("#popupImage").on('swiperight', function () {
-        switch (true) {
-          case counter == 1:
+        $("#popupImage").on("swipeleft", function() {
+          switch (true) {
+            case counter == 1:
+              counter--;
+              $("#popupImage").css(
+                "background-image",
+                `url(' ${getImage(counter)}')`
+              );
+              $("#right").fadeOut();
 
-            counter++;
-            $("#popupImage").css('background-image', `url(' ${getImage(counter)}')`)
-            $("#left").fadeOut();
-            break;
+              break;
 
-          case counter == 0:
+            case counter == 2:
+              counter--;
+              $("#popupImage").css(
+                "background-image",
+                `url(' ${getImage(counter)}')`
+              );
+              $("#left").fadeIn();
+              $("#right").fadeIn();
 
-            counter++;
-            $("#popupImage").css('background-image', `url(' ${getImage(counter)}')`)
-            $("#left").fadeIn();
-            $("#right").fadeIn();
-            break;
+              break;
 
-          case counter == 2:
-            break;
-        }
-      })
-      $("#popupImage").on("popupafterclose", function () {
-        $("#left").fadeIn();
-        $("#right").fadeIn();
-        counter = 1;
-      });
-    }, "json").fail(function (status) {
+            case counter == 0:
+              break;
+          }
+        });
+        $("#popupImage").on("swiperight", function() {
+          switch (true) {
+            case counter == 1:
+              counter++;
+              $("#popupImage").css(
+                "background-image",
+                `url(' ${getImage(counter)}')`
+              );
+              $("#left").fadeOut();
+              break;
+
+            case counter == 0:
+              counter++;
+              $("#popupImage").css(
+                "background-image",
+                `url(' ${getImage(counter)}')`
+              );
+              $("#left").fadeIn();
+              $("#right").fadeIn();
+              break;
+
+            case counter == 2:
+              break;
+          }
+        });
+        $("#popupImage").on("popupafterclose", function() {
+          $("#left").fadeIn();
+          $("#right").fadeIn();
+          counter = 1;
+        });
+      },
+      "json"
+    ).fail(function(status) {
       console.log(status.status + " error. There was an error retreiving data");
     });
 
-
-    $( "#close" ).on("tap", function (){
-      $("#popupImage").popup( "close" )
-    })
+    $("#close").on("tap", function() {
+      $("#popupImage").popup("close");
+    });
 
     function getMarked(postid) {
-
-      const allfaves = getFaves()
+      const allfaves = getFaves();
       const marked = allfaves.some(f => f.id == postid);
 
       if (marked) {
@@ -128,46 +136,42 @@ $(document).on("pagecontainerbeforeshow", function (e, ui) {
         $("#favourited").hide();
       }
     }
-
-
   }
 });
 
 // TO CLOSE
-// 
+//
 
-
-
-
-$(document).on("pagecontainerbeforeshow", function (e, ui) {
-  const thisPage = $.mobile.pageContainer.pagecontainer('getActivePage').attr('id');
-  if (thisPage == 'favourites') {
-
-
-    const faves = getFaves()
-    console.log(faves)
+$(document).on("pagecontainerbeforeshow", function(e, ui) {
+  const thisPage = $.mobile.pageContainer
+    .pagecontainer("getActivePage")
+    .attr("id");
+  if (thisPage == "favourites") {
+    const faves = getFaves();
+    console.log(faves);
     let text = "";
-    $.each(faves, function (i, v) {
+    $.each(faves, function(i, v) {
       text += `<div class="item"><li> <a  href="orchard.html?post=${
-    v.id
-  }" data-transition="slidefade"> 
+        v.id
+      }" data-transition="slidefade"> 
       ${v.name} 
       </a></li>`;
-      text += `<i class="fa fa-trash-o" aria-hidden="true" id="${v.id}"></i></div>`;
+      text += `<i class="fa fa-trash-o" aria-hidden="true" id="${
+        v.id
+      }"></i></div>`;
     });
-    $('#fave-list').html(text)
+    $("#fave-list").html(text);
 
-    $('.fa-trash-o').on('tap', function () {
-      var theId = $(this).attr('id')
-      $(this).parent().remove()
-      console.log(theId)
-      removeFaves(theId)
-    })
-
-
+    $(".fa-trash-o").on("tap", function() {
+      var theId = $(this).attr("id");
+      $(this)
+        .parent()
+        .remove();
+      console.log(theId);
+      removeFaves(theId);
+    });
   }
-
-})
+});
 
 // Helper functions
 function addFaves(fave) {
@@ -187,7 +191,6 @@ function removeFaves(id) {
 
   localStorage.setItem("faves", JSON.stringify(amendedFaves));
 }
-
 
 function getFaves() {
   let faves;
